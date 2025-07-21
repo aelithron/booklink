b36d2a972ea87eec08acdfaabafee6bb28b9ee7b
Aelithron <aelithron@gmail.com>
2025-07-20 19:01:25 -0600

    Finish the last commit Forgot to stage my changes!

--- app/bookForm.module.tsx
+++ app/bookForm.module.tsx
@@ -1,48 +1,61 @@
 "use client";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
-import { FormEvent, ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 export default function BookForm() {
-  const router = useRouter();
   const [book, setBook] = useState<string>('');
-  function onSubmit(e: FormEvent) {
-    e.preventDefault();
-    router.push("/" + book);
-  }
   return (
-    <form onSubmit={onSubmit} className="flex flex-col items-center">
+    <div className="flex flex-col items-center">
       <label className="text-sm font-semibold mb-1">Book title or ISBN:</label>
-      <div className="flex gap-1">
-        <input value={book} onChange={(e) => setBook(e.target.value)} className="bg-slate-500 border-2 border-slate-700 rounded-lg px-2 py-1" />
-        <button type="submit" className="bg-slate-500 border-slate-700 border-2 rounded-xl px-2 py-1 hover:text-sky-500"><FontAwesomeIcon icon={faArrowRight} /></button>
-      </div>
+      <input value={book} onChange={(e) => setBook(e.target.value)} className="bg-slate-500 border-2 border-slate-700 rounded-lg px-2 py-1" />
       <AutocompleteForm bookSearch={book} />
-    </form>
+    </div>
   );
 }
 
 function AutocompleteForm({ bookSearch }: { bookSearch: string }): ReactNode {
+  const router = useRouter();
   const [bookData, setBookData] = useState<AutoCompletedBook[]>();
+  function selectBook(id: string) {
+    fetch(`/api/create`, { method: "POST", body: JSON.stringify({ id: id }) })
+      .then((res) => {
+        if (!res) {
+          alert("Error resolving book!");
+          return null;
+        }
+        return res.json();
+      })
+      .then((res) => {
+        if (res.error) {
+          alert(`Error resolving book: ${res.message} (${res.error})`);
+          return;
+        }
+        router.push(`/${res.id}`);
+      });
+  }
   useEffect(() => {
     if (!bookSearch || bookSearch === "") return;
     fetch(`${window.location.origin}/api/autocomplete`, { method: "POST", body: JSON.stringify({ search: bookSearch }) })
       .then((res) => res.json())
       .then((jsonRes) => {
-        setBookData(jsonRes);
-      })
+        setBookData(jsonRes ?
+          jsonRes.filter((book: AutoCompletedBook, index: number, self: AutoCompletedBook[]) =>
+            index === self.findIndex((b) => b.id === book.id)
+          ) : []
+        );
+      });
   }, [bookSearch]);
-  if (!bookSearch || bookSearch === "") return null;
   if (!bookData || bookData.length < 1) return <p className="flex flex-col p-2 bg-slate-500 border-2 mt-4 rounded-xl border-slate-700">Loading...</p>
   return (
     <div className="flex flex-col p-2 bg-slate-500 border-2 mt-4 rounded-xl border-slate-700">
-      {bookData.map((book) => <div key={book.id} className="grid grid-cols-5 p-1 items-center w-[50vw]">
-        <img src={book.cover} width={60} height={200} />
-        <div className="col-span-4 flex flex-col">
-          <h2 className="text-lg">{truncateTitle(book.title)}</h2>
-          <h3>{book.author}</h3>
-        </div>
+      {bookData.map((book) => <div key={book.id}>
+        <button onClick={() => selectBook(book.id)} className="grid grid-cols-5 p-1 items-center w-[50vw]">
+          <img src={book.cover} width={60} height={200} />
+          <div className="col-span-4 flex flex-col">
+            <h2 className="text-lg font-semibold">{truncateTitle(book.title)}</h2>
+            <h3>{book.author}</h3>
+          </div>
+        </button>
       </div>)}
     </div>
   )
@@ -54,6 +67,7 @@ export type AutoCompletedBook = {
   author: string
 }
 export function truncateTitle(text: string): string {
+  if (text == null) return "";
   const maxLength = 35;
   if (text.length <= maxLength) return text;
   return text.slice(0, maxLength) + '...';
